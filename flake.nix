26c2f0d9ffa3ce7217e52519676c1f6e0943e9a9
Ifiok Jr <ifiokotung@gmail.com>
2023-01-23 14:13:18 +0000

    chore: add temporary nix command

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "an atomic css compiler"
+  description = "an atomic css compiler";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  }
+  # inputs = {
+  #   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  # }
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+    packages.x86_64-darwin.default = self.packages.x86_64-darwin.hello;
   };
 }
