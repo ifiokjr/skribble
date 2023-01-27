1b9995cb8f99e460651582ecec7163791c96ccf3
Ifiok Jr <ifiokotung@gmail.com>
2023-01-27 07:32:11 +0000

    ci: load environment from nix

--- devenv.nix
+++ devenv.nix
@@ -61,5 +61,24 @@
     rm -rf .vscode
     cp -r ./setup/editors/vscode .vscode
   '';
+  scripts."setup:ci".exec = ''
+    # update GitHub CI Path
+    echo "$DEVENV_PROFILE/bin" >> $GITHUB_PATH
+    echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
 
+    # prepend common compilation lookup paths
+    echo PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> $GITHUB_ENV
+    echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $GITHUB_ENV
+    echo LIBRARY_PATH=$LIBRARY_PATH" >> $GITHUB_ENV
+    echo C_INCLUDE_PATH=$C_INCLUDE_PATH" >> $GITHUB_ENV
+
+    # these provide shell completions / default config options
+    echo XDG_DATA_DIRS=$XDG_DATA_DIRS" >> $GITHUB_ENV
+    echo XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS" >> $GITHUB_ENV
+
+    echo DEVENV_DOTFILE=$DEVENV_DOTFILE" >> $GITHUB_ENV
+    echo DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
+    echo DEVENV_ROOT=$DEVENV_ROOT" >> $GITHUB_ENV
+    echo DEVENV_STATE=$DEVENV_STATE" >> $GITHUB_ENV
+  '';
 }
