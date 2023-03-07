5b8cf13370975889eeff79f0278939a7d3fac7bb
Ifiok Jr <ifiokotung@gmail.com>
2023-03-07 17:20:41 +0000

    feat: allow `other` json fields in config
    - Closes #21

--- crates/skribble_core/src/config.rs
+++ crates/skribble_core/src/config.rs
@@ -4,6 +4,7 @@ use std::ops::DerefMut;
 use indexmap::IndexMap;
 use serde::Deserialize;
 use serde::Serialize;
+use serde_json::Value;
 use typed_builder::TypedBuilder;
 
 use crate::Error;
@@ -15,6 +16,8 @@ use crate::Result;
 pub struct StyleConfig {
   /// The general options.
   pub options: Options,
+  /// Setup the keyframes.
+  pub keyframes: Keyframes,
   /// Setup the media queries.
   pub media_queries: MediaQueries,
   /// Modifiers are used to nest styles within a selector.
@@ -26,10 +29,12 @@ pub struct StyleConfig {
   pub rules: NamedRules,
   /// Named classes.
   pub classes: NamedClasses,
-  /// Setup the keyframes.
-  pub keyframes: Keyframes,
   /// Hardcoded colors for the pallette.
   pub palette: Palette,
+  /// Support additional fields for plugins.
+  #[serde(flatten, skip_serializing_if = "Option::is_none", default = "None")]
+  #[builder(default, setter(into, strip_option))]
+  pub additional_fields: Option<AdditionalFields>,
 }
 
 impl StyleConfig {
@@ -45,6 +50,203 @@ impl StyleConfig {
 
   pub fn to_pretty_json(&self) -> Result<String> {
     serde_json::to_string_pretty(self).map_err(|source| Error::CouldNotSerializeConfig { source })
+  }
+}
+
+/// Options to use in the configuration.
+#[derive(Serialize, Deserialize, TypedBuilder, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Options {
+  /// This is the default format of colors rendered in css.
+  #[serde(default)]
+  pub color_format: ColorFormat,
+
+  /// By default there is no variable prefix.
+  #[serde(default = "default_variable_prefix")]
+  pub variable_prefix: String,
+}
+
+impl Default for Options {
+  fn default() -> Self {
+    Self {
+      color_format: Default::default(),
+      variable_prefix: default_variable_prefix(),
+    }
+  }
+}
+
+fn default_variable_prefix() -> String {
+  "sk".to_string()
+}
+
+/// ColorFormat is used to determine the default format of the colors.
+#[derive(Default, Serialize, Deserialize, Debug, PartialEq)]
+pub enum ColorFormat {
+  #[serde(rename = "rgb")]
+  Rgb,
+  #[serde(rename = "hsl")]
+  #[default]
+  Hsl,
+}
+
+/// This setups up the animation keyframes for the configuration. The names can
+/// be reference in the atoms.
+///
+/// ```json
+/// {
+///   "keyframes": {
+///     "enter": {
+///       "from": {
+///         "opacity": "var(--enter-opacity, 1)",
+///         "transform": "translate3d(var(--enter-translate-x, 0), var(--enter-translate-y, 0), 0) scale3d(var(--enter-scale, 1), var(--enter-scale, 1), var(--enter-scale, 1)) rotate(var(--enter-rotate, 0))"
+///       }
+///     },
+///     "exit": {
+///       "to": {
+///         "opacity": "var(--exit-opacity, 1)",
+///         "transform": "translate3d(var(--exit-translate-x, 0), var(--exit-translate-y, 0), 0) scale3d(var(--exit-scale, 1), var(--exit-scale, 1), var(--exit-scale, 1)) rotate(var(--exit-rotate, 0))"
+///       }
+///     },
+///     "spin": {
+///       "from": { "transform": "rotate(0deg)" },
+///       "to": { "transform": "rotate(360deg)" }
+///     },
+///     "ping": { "75%, 100%": { "transform": "scale(2)", "opacity": "0" } },
+///     "pulse": { "0%, 100%": { "opacity": "1" }, "50%": { "opacity": "0.5" } },
+///     "bounce": {
+///       "0%, 100%": {
+///         "transform": "translateY(-25%)",
+///         "animationTimingFunction": "cubic-bezier(0.8, 0, 1, 1)"
+///       },
+///       "50%": {
+///         "transform": "translateY(0)",
+///         "animationTimingFunction": "cubic-bezier(0, 0, 0.2, 1)"
+///       }
+///     }
+///   }
+/// }
+/// ```
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone)]
+pub struct Keyframes(Frames);
+type Frames = IndexMap<String, IndexMap<String, String>>;
+
+impl<K, C, V, I> From<I> for Keyframes
+where
+  K: Into<String>,
+  C: Into<String>,
+  V: IntoIterator<Item = (K, C)>,
+  I: IntoIterator<Item = (K, V)>,
+{
+  fn from(iter: I) -> Self {
+    let frames = iter
+      .into_iter()
+      .map(|(name, value)| {
+        (
+          name.into(),
+          value
+            .into_iter()
+            .map(|(key, value)| (key.into(), value.into()))
+            .collect(),
+        )
+      })
+      .collect();
+
+    Self(frames)
+  }
+}
+
+impl<K, C, V> FromIterator<(K, V)> for Keyframes
+where
+  K: Into<String>,
+  C: Into<String>,
+  V: IntoIterator<Item = (K, C)>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
+  }
+}
+
+impl Deref for Keyframes {
+  type Target = Frames;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for Keyframes {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
+/// Media queries can should be defined as a map of names to their css queries.
+///
+/// ```json
+/// {
+///   "mediaQueries": {
+///     "sm": "(min-width: 640px)",
+///     "md": "(min-width: 768px)",
+///     "lg": "(min-width: 1024px)",
+///     "xl": "(min-width: 1280px)",
+///     "xxl": "(min-width: 1536px)",
+///     "portrait": "(orientation: portrait)",
+///     "combined": "(min-width: 30em) and (orientation: landscape)"
+///   }
+/// }
+/// ```
+#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
+pub struct MediaQueries(IndexMap<String, String>);
+
+impl MediaQueries {
+  pub fn breakpoint(
+    &mut self,
+    name: impl Into<String>,
+    value: impl Into<String>,
+  ) -> Option<String> {
+    let query = format!("(min-width: {})", value.into());
+    self.0.insert(name.into(), query)
+  }
+}
+
+impl<K, V, I> From<I> for MediaQueries
+where
+  K: Into<String>,
+  V: Into<String>,
+  I: IntoIterator<Item = (K, V)>,
+{
+  fn from(value: I) -> Self {
+    let mut breakpoints = IndexMap::new();
+
+    for (name, value) in value {
+      breakpoints.insert(name.into(), value.into());
+    }
+
+    Self(breakpoints)
+  }
+}
+
+impl<K, V> FromIterator<(K, V)> for MediaQueries
+where
+  K: Into<String>,
+  V: Into<String>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
+  }
+}
+
+impl Deref for MediaQueries {
+  type Target = IndexMap<String, String>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for MediaQueries {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
   }
 }
 
@@ -103,6 +305,31 @@ where
   }
 }
 
+impl<K, C, V> FromIterator<(K, V)> for NamedRules
+where
+  K: Into<String>,
+  C: Into<String>,
+  V: IntoIterator<Item = (K, Option<C>)>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
+  }
+}
+
+impl Deref for NamedRules {
+  type Target = IndexMap<String, IndexMap<String, Option<String>>>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for NamedRules {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
 /// The named classes with their own defined values.
 ///
 /// ```json
@@ -141,97 +368,26 @@ where
   }
 }
 
-/// Options to use in the configuration.
-#[derive(Serialize, Deserialize, TypedBuilder, Debug, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct Options {
-  /// This is the default format of colors rendered in css.
-  #[serde(default)]
-  pub color_format: ColorFormat,
-
-  /// By default there is no variable prefix.
-  #[serde(default = "default_variable_prefix")]
-  pub variable_prefix: String,
-}
-
-impl Default for Options {
-  fn default() -> Self {
-    Self {
-      color_format: Default::default(),
-      variable_prefix: default_variable_prefix(),
-    }
-  }
-}
-
-fn default_variable_prefix() -> String {
-  "sk".to_string()
-}
-
-/// ColorFormat is used to determine the default format of the colors.
-#[derive(Default, Serialize, Deserialize, Debug, PartialEq)]
-pub enum ColorFormat {
-  #[serde(rename = "rgb")]
-  Rgb,
-  #[serde(rename = "hsl")]
-  #[default]
-  Hsl,
-}
-
-/// Media queries can should be defined as a map of names to their css queries.
-///
-/// ```json
-/// {
-///   "mediaQueries": {
-///     "sm": "(min-width: 640px)",
-///     "md": "(min-width: 768px)",
-///     "lg": "(min-width: 1024px)",
-///     "xl": "(min-width: 1280px)",
-///     "xxl": "(min-width: 1536px)",
-///     "portrait": "(orientation: portrait)",
-///     "combined": "(min-width: 30em) and (orientation: landscape)"
-///   }
-/// }
-/// ```
-#[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize)]
-pub struct MediaQueries(IndexMap<String, String>);
-
-impl MediaQueries {
-  pub fn breakpoint(
-    &mut self,
-    name: impl Into<String>,
-    value: impl Into<String>,
-  ) -> Option<String> {
-    let query = format!("(min-width: {})", value.into());
-    self.0.insert(name.into(), query)
-  }
-}
-
-impl<K, V, I> From<I> for MediaQueries
+impl<K, C, V> FromIterator<(K, V)> for NamedClasses
 where
   K: Into<String>,
-  V: Into<String>,
-  I: IntoIterator<Item = (K, V)>,
+  C: Into<String>,
+  V: IntoIterator<Item = (K, C)>,
 {
-  fn from(value: I) -> Self {
-    let mut breakpoints = IndexMap::new();
-
-    for (name, value) in value {
-      breakpoints.insert(name.into(), value.into());
-    }
-
-    Self(breakpoints)
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
   }
 }
 
-impl Deref for MediaQueries {
-  type Target = IndexMap<String, String>;
+impl Deref for NamedClasses {
+  type Target = IndexMap<String, IndexMap<String, String>>;
 
   fn deref(&self) -> &Self::Target {
     &self.0
   }
 }
 
-impl DerefMut for MediaQueries {
+impl DerefMut for NamedClasses {
   fn deref_mut(&mut self) -> &mut Self::Target {
     &mut self.0
   }
@@ -289,6 +445,16 @@ where
   }
 }
 
+impl<K, V> FromIterator<(K, V)> for Palette
+where
+  K: Into<String>,
+  V: Into<String>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
+  }
+}
+
 impl Deref for Palette {
   type Target = IndexMap<String, String>;
 
@@ -298,61 +464,6 @@ impl Deref for Palette {
 }
 
 impl DerefMut for Palette {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.0
-  }
-}
-
-type Frames = IndexMap<String, IndexMap<String, String>>;
-/// This setups up the animation keyframes for the configuration. The names can
-/// be reference in the atoms.
-///
-/// ```json
-/// {
-///   "keyframes": {
-///     "enter": {
-///       "from": {
-///         "opacity": "var(--enter-opacity, 1)",
-///         "transform": "translate3d(var(--enter-translate-x, 0), var(--enter-translate-y, 0), 0) scale3d(var(--enter-scale, 1), var(--enter-scale, 1), var(--enter-scale, 1)) rotate(var(--enter-rotate, 0))"
-///       }
-///     },
-///     "exit": {
-///       "to": {
-///         "opacity": "var(--exit-opacity, 1)",
-///         "transform": "translate3d(var(--exit-translate-x, 0), var(--exit-translate-y, 0), 0) scale3d(var(--exit-scale, 1), var(--exit-scale, 1), var(--exit-scale, 1)) rotate(var(--exit-rotate, 0))"
-///       }
-///     },
-///     "spin": {
-///       "from": { "transform": "rotate(0deg)" },
-///       "to": { "transform": "rotate(360deg)" }
-///     },
-///     "ping": { "75%, 100%": { "transform": "scale(2)", "opacity": "0" } },
-///     "pulse": { "0%, 100%": { "opacity": "1" }, "50%": { "opacity": "0.5" } },
-///     "bounce": {
-///       "0%, 100%": {
-///         "transform": "translateY(-25%)",
-///         "animationTimingFunction": "cubic-bezier(0.8, 0, 1, 1)"
-///       },
-///       "50%": {
-///         "transform": "translateY(0)",
-///         "animationTimingFunction": "cubic-bezier(0, 0, 0.2, 1)"
-///       }
-///     }
-///   }
-/// }
-/// ```
-#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone)]
-pub struct Keyframes(Frames);
-
-impl Deref for Keyframes {
-  type Target = Frames;
-
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
-}
-
-impl DerefMut for Keyframes {
   fn deref_mut(&mut self) -> &mut Self::Target {
     &mut self.0
   }
@@ -406,6 +517,17 @@ where
       .collect();
 
     Self(parent_modifiers)
+  }
+}
+
+impl<K, C, V> FromIterator<(K, V)> for ParentModifiers
+where
+  K: Into<String>,
+  C: Into<String>,
+  V: IntoIterator<Item = C>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
   }
 }
 
@@ -479,6 +601,49 @@ impl DerefMut for Modifiers {
   }
 }
 
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone)]
+pub struct AdditionalFields(IndexMap<String, Value>);
+
+impl<K, V, I> From<I> for AdditionalFields
+where
+  K: Into<String>,
+  V: Into<Value>,
+  I: IntoIterator<Item = (K, V)>,
+{
+  fn from(iter: I) -> Self {
+    let additional_fields = iter
+      .into_iter()
+      .map(|(k, v)| (k.into(), v.into()))
+      .collect();
+
+    Self(additional_fields)
+  }
+}
+
+impl<K, V> FromIterator<(K, V)> for AdditionalFields
+where
+  K: Into<String>,
+  V: Into<Value>,
+{
+  fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+    Self::from(iter)
+  }
+}
+
+impl Deref for AdditionalFields {
+  type Target = IndexMap<String, Value>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for AdditionalFields {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -487,11 +652,18 @@ mod tests {
   fn check_config_can_serialize() {
     let config: StyleConfig = Default::default();
     let json = config.to_json().unwrap();
-    assert_eq!(config, serde_json::from_str(&json).unwrap());
+    StyleConfig::from_json(json).unwrap();
   }
 
   #[test]
   fn default_config() {
     insta::assert_json_snapshot!(StyleConfig::default());
+  }
+
+  #[test]
+  fn supports_additional_fields() {
+    let json = include_str!("default.json");
+    let config = StyleConfig::from_json(json).unwrap();
+    insta::assert_json_snapshot!(config);
   }
 }
