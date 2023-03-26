32f27ac6616b3e2a85156dc15cd95c7d3d78ee3f
Ifiok Jr <ifiokotung@gmail.com>
2023-03-26 09:42:34 +0100

    feat: add `Classes` struct

--- crates/skribble_core/src/css/class.rs
+++ crates/skribble_core/src/css/class.rs
@@ -1,6 +1,8 @@
 use std::cmp::Ordering;
 use std::hash::Hash;
 use std::hash::Hasher;
+use std::ops::Deref;
+use std::ops::DerefMut;
 
 use indexmap::IndexSet;
 use serde::Deserialize;
@@ -123,5 +125,48 @@ impl PartialOrd for Class {
 impl Ord for Class {
   fn cmp(&self, other: &Self) -> Ordering {
     self.score.cmp(&other.score)
+  }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
+pub struct Classes(IndexSet<Class>);
+
+impl Classes {
+  pub fn merge(&mut self, other: impl Into<Self>) {
+    self.0.extend(other.into().0);
+    self.sort_by_class();
+  }
+
+  pub fn sort_by_class(&mut self) {
+    self.0.sort_by(|a, b| a.cmp(b));
+  }
+}
+
+impl IntoIterator for Classes {
+  type IntoIter = <IndexSet<Class> as IntoIterator>::IntoIter;
+  type Item = Class;
+
+  fn into_iter(self) -> Self::IntoIter {
+    self.0.into_iter()
+  }
+}
+
+impl FromIterator<Class> for Classes {
+  fn from_iter<T: IntoIterator<Item = Class>>(iter: T) -> Self {
+    Self(iter.into_iter().collect())
+  }
+}
+
+impl Deref for Classes {
+  type Target = IndexSet<Class>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for Classes {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
   }
 }
