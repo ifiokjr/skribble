c769762aa7be43d7a2837690c030b64b69c25065
Ifiok Jr <ifiokotung@gmail.com>
2023-03-26 06:48:17 +0100

    chore: remove `colorsys`

--- crates/skribble_core/src/utils.rs
+++ crates/skribble_core/src/utils.rs
@@ -1,9 +1,6 @@
 use std::path::Path;
-use std::str::FromStr;
 
-use colorsys::ColorAlpha;
-use colorsys::Hsl;
-use colorsys::Rgb;
+
 use globset::Glob;
 use globset::GlobSet;
 use globset::GlobSetBuilder;
@@ -15,7 +12,7 @@ use walkdir::WalkDir;
 
 use crate::constants::INDENTATION;
 use crate::AnyResult;
-use crate::Palette;
+
 
 lazy_static! {
   static ref ESCAPE_CSS_STRING_REGEX: Regex =
@@ -79,62 +76,6 @@ pub fn indent(content: impl AsRef<str>, style: IndentStyle) -> String {
   }
 
   result.trim_end().to_string()
-}
-
-/// Convert the color to a valid css value with the opacity set to the provided
-/// css variable.
-pub fn convert_css_value_to_color(
-  original: impl AsRef<str>,
-  palette: &Palette,
-  opacity: impl AsRef<str>,
-) -> String {
-  // Create a copy of the string value to search against.
-  let mut string_value = original.as_ref().to_string();
-
-  if let Some(derived_value) = palette.get(&string_value) {
-    string_value = derived_value.clone();
-  }
-
-  get_rgba_color_from_string(&string_value, opacity)
-}
-
-/// Will return the string unchanged if the color provided is not valid.
-pub fn get_rgba_color_from_string(value: impl AsRef<str>, opacity: impl AsRef<str>) -> String {
-  let mut string_value = value.as_ref().to_string();
-
-  let rgb = if let Some(stripped) = string_value.strip_prefix('#') {
-    Rgb::from_hex_str(stripped).ok()
-  } else if string_value.starts_with("rgb") {
-    Rgb::from_str(&string_value).ok()
-  } else if string_value.starts_with("hsl") {
-    if let Ok(hsl) = Hsl::from_str(&string_value) {
-      Some(Rgb::from(hsl))
-    } else {
-      None
-    }
-  } else {
-    None
-  };
-
-  let wrapped_opacity = wrap_css_variable(opacity);
-
-  if let Some(rgb) = rgb {
-    let alpha = if rgb.alpha() < 1.0 {
-      format!("calc({} * {})", rgb.alpha(), wrapped_opacity)
-    } else {
-      wrapped_opacity
-    };
-
-    string_value = format!(
-      "rgba({}, {}, {}, {})",
-      rgb.red(),
-      rgb.green(),
-      rgb.blue(),
-      alpha
-    );
-  }
-
-  string_value
 }
 
 /// Wrap the opacity value in `var()` if not already done in the config.
