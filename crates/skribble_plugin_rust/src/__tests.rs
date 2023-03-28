376483ded90f0d704a075a2241a5f2a6dbf62db0
Ifiok Jr <ifiokotung@gmail.com>
2023-03-28 12:25:47 +0100

    test: refactor test location

---
+++ crates/skribble_plugin_rust/src/__tests.rs
@@ -0,0 +1,24 @@
+use skribble_core::*;
+use skribble_preset_default::PresetDefault;
+
+use super::*;
+
+#[test]
+fn default_can_be_added_to_runner() {
+  let default_preset = PresetDefault::builder().build();
+  let rust_plugin = RustPlugin::builder().build();
+
+  let config: StyleConfig = StyleConfig::builder()
+    .plugins(vec![
+      PluginContainer::from(default_preset),
+      PluginContainer::from(rust_plugin),
+    ])
+    .build();
+
+  let mut runner = SkribbleRunner::new(config);
+  let _ = runner.initialize();
+  let result = runner.generate().unwrap();
+  let generated = result.get(0).unwrap();
+  let content = &generated.content;
+  insta::assert_display_snapshot!(content);
+}
